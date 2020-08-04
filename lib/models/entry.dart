9a1afe0159ff021af82edc740e08919b1a28d54f
Akshit Garg <garg.akshit@gmail.com>
2020-08-05 02:13:32 +0530

    [Models] Complete the Entry and the Otp models

--- lib/models/entry.dart
+++ lib/models/entry.dart
@@ -1,3 +1,5 @@
+import 'package:passwd/models/otp.dart';
+
 class Entry {
   String name;
   String username;
@@ -5,4 +7,39 @@ class Entry {
   String note;
   String uri;
   String favicon;
+  Otp otp;
+
+  Entry({
+    this.name,
+    this.username,
+    this.password,
+    this.note,
+    this.uri,
+    this.favicon,
+    this.otp,
+  });
+
+  Entry.fromJson(Map<String, dynamic> json) {
+    name = json['name'];
+    username = json['username'];
+    password = json['password'];
+    note = json['note'];
+    uri = json['uri'];
+    favicon = json['favicon'];
+    otp = json['otp'] != null ? new Otp.fromJson(json['otp']) : null;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['name'] = this.name;
+    data['username'] = this.username;
+    data['password'] = this.password;
+    data['note'] = this.note;
+    data['uri'] = this.uri;
+    data['favicon'] = this.favicon;
+    if (this.otp != null) {
+      data['otp'] = this.otp.toJson();
+    }
+    return data;
+  }
 }
--- lib/models/otp.dart
+++ lib/models/otp.dart
@@ -1,13 +1,14 @@
 class Otp {
-  String type = "totp";
-  String issuer = "Generic Issuer";
-  String algorithm = "SHA1";
+  String type;
+  String issuer;
+  String algorithm;
   String secret;
   String account;
-  int digits = 6;
-  int timeout = 30;
+  int digits;
+  int timeout;
 
   Otp({
+    this.type,
     this.issuer,
     this.algorithm,
     this.secret,
@@ -16,12 +17,25 @@ class Otp {
     this.timeout,
   });
 
-  static Map toMap(Otp otp) {
-    return {
-      "type": otp.type,
-      "issuer": otp.issuer,
-      "algorithm": otp.algorithm,
-      "account": otp.account,
-    };
+  Otp.fromJson(Map<String, dynamic> json) {
+    type = json['type'];
+    issuer = json['issuer'];
+    algorithm = json['algorithm'];
+    secret = json['secret'];
+    account = json['account'];
+    digits = json['digits'];
+    timeout = json['timeout'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['type'] = this.type;
+    data['issuer'] = this.issuer;
+    data['algorithm'] = this.algorithm;
+    data['secret'] = this.secret;
+    data['account'] = this.account;
+    data['digits'] = this.digits;
+    data['timeout'] = this.timeout;
+    return data;
   }
 }
