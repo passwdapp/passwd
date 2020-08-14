dc3592100c0d9683ce1215fe0be6a77386c19b33
Akshit Garg <garg.akshit@gmail.com>
2020-08-15 00:53:44 +0530

    [AuthenticationService] Add allow_biometrics
    Well, biometrics was opt-in, and I basically forgot that, so started to work on it

--- lib/services/authentication/authentication_impl.dart
+++ lib/services/authentication/authentication_impl.dart
@@ -3,10 +3,12 @@ import 'package:passwd/services/authentication/authentication_service.dart';
 import 'package:passwd/services/crypto/crypto_service.dart';
 import 'package:passwd/services/locator.dart';
 import 'package:passwd/services/secure_kv/secure_kv.dart';
+import 'package:supercharged/supercharged.dart';
 
 @LazySingleton(as: AuthenticationService)
 class AuthenticationImpl implements AuthenticationService {
   final String key = "ENCRYPTION_KEY";
+  final String biometricsKey = "ALLOW_BIOMETRICS";
 
   final CryptoService crypto = locator<CryptoService>();
   final SecureKVService kv = locator<SecureKVService>();
@@ -28,5 +30,15 @@ class AuthenticationImpl implements AuthenticationService {
   Future writePin(int pin) async {
     String encryptionKey = crypto.sha512(pin.toString());
     await kv.putValue(key, encryptionKey);
+  }
+
+  @override
+  Future<bool> allowBiometrics() async {
+    return (await kv.getValue(biometricsKey)).toInt() == 1 ? true : false;
+  }
+
+  @override
+  Future writeBiometrics(bool allow) async {
+    await kv.putValue(biometricsKey, allow ? "1" : "0");
   }
 }
--- lib/services/authentication/authentication_service.dart
+++ lib/services/authentication/authentication_service.dart
@@ -1,5 +1,7 @@
 abstract class AuthenticationService {
   Future writePin(int pin);
+  Future writeBiometrics(bool allow);
   Future<String> readEncryptionKey();
   Future<bool> comparePin(int pin);
+  Future<bool> allowBiometrics();
 }
