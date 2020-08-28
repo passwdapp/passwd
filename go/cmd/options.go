f6492c9d7120bf3d67dd7e8a54c4f9d7c9e4851e
Akshit Garg <garg.akshit@gmail.com>
2020-08-28 20:01:59 +0530

    [Desktop] Add plugins in options.go
    I will require path_provider and shared_preferences (in place of flutter_secure_storage). Well not using a secure storage won't probably be an issue, as the pin and stuff are never stored in plain text

--- go/cmd/options.go
+++ go/cmd/options.go
@@ -2,8 +2,18 @@ package main
 
 import (
 	"github.com/go-flutter-desktop/go-flutter"
+	"github.com/go-flutter-desktop/plugins/path_provider"
+	"github.com/go-flutter-desktop/plugins/shared_preferences"
 )
 
 var options = []flutter.Option{
-	flutter.WindowInitialDimensions(800, 1280),
+	flutter.WindowInitialDimensions(800, 475),
+	flutter.AddPlugin(&path_provider.PathProviderPlugin{
+		VendorName:      "gargakshit",
+		ApplicationName: "passwd",
+	}),
+	flutter.AddPlugin(&shared_preferences.SharedPreferencesPlugin{
+		VendorName:      "gargakshit",
+		ApplicationName: "passwd",
+	}),
 }
--- go/go.mod
+++ go/go.mod
@@ -4,5 +4,7 @@ go 1.15
 
 require (
 	github.com/go-flutter-desktop/go-flutter v0.41.2
+	github.com/go-flutter-desktop/plugins/path_provider v0.4.0
+	github.com/go-flutter-desktop/plugins/shared_preferences v0.4.3
 	github.com/pkg/errors v0.9.1
 )
