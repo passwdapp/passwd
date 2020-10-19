cdf24f1d00e3878820981b7193976518f5000b99
Akshit Garg <garg.akshit@gmail.com>
2020-10-20 00:38:07 +0530

    [GeneratePasswordScreen] Move off stacked

--- lib/screens/generate_password/generate_password_screen.dart
+++ lib/screens/generate_password/generate_password_screen.dart
@@ -1,169 +1,212 @@
 import 'package:ez_localization/ez_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_icons/flutter_icons.dart';
-import 'package:stacked/stacked.dart';
 import 'package:supercharged/supercharged.dart';
 
+import '../../services/locator.dart';
+import '../../services/password/password_service.dart';
 import '../../widgets/button.dart';
-import 'generate_password_viewmodel.dart';
 
-class GeneratePasswordScreen extends HookWidget {
+class GeneratePasswordScreen extends StatefulWidget {
+  @override
+  _GeneratePasswordScreenState createState() => _GeneratePasswordScreenState();
+}
+
+class _GeneratePasswordScreenState extends State<GeneratePasswordScreen> {
+  TextEditingController wordController = TextEditingController.fromValue(
+    TextEditingValue(text: "5"),
+  );
+
+  String password = "";
+  int words = 5;
+  bool capitalize = true;
+  bool diceware = true;
+
+  Future getPassword({
+    int length = 5,
+    bool capitalize = true,
+  }) async {
+    if (diceware) {
+      String generatedPassword =
+          await locator<PasswordService>().generateDicewarePassword(
+        words: length,
+        capitalize: capitalize,
+      );
+      setState(() {
+        password = generatedPassword;
+      });
+    } else {
+      String generatedPassword =
+          locator<PasswordService>().generateRandomPassword(
+        length: length,
+      );
+      setState(() {
+        password = generatedPassword;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    wordController.addListener(() {
+      setState(() {
+        words = wordController.text.toInt();
+      });
+    });
+
+    getPassword(
+      length: words,
+      capitalize: capitalize,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    TextEditingController wordController = useTextEditingController.fromValue(
-      TextEditingValue(text: "5"),
-    );
-
-    return ViewModelBuilder<GeneratePasswordViewModel>.reactive(
-      viewModelBuilder: () => GeneratePasswordViewModel(),
-      onModelReady: (model) {
-        model.getPassword(
-          length: model.words,
-          capitalize: model.capitalize,
-        );
-
-        wordController.addListener(() {
-          model.words = wordController.text.toInt();
-        });
-      },
-      builder: (context, model, child) => Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: Text(
-            context.getString("password"),
-            style: TextStyle(
-              letterSpacing: 1.25,
-              fontSize: 18,
-            ),
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          context.getString("password"),
+          style: TextStyle(
+            letterSpacing: 1.25,
+            fontSize: 18,
           ),
-          leading: IconButton(
-            onPressed: () {
-              model.pop();
-            },
-            tooltip: context.getString("back_tooltip"),
-            icon: Icon(Feather.x_circle),
-          ),
-          actions: [
-            IconButton(
-              onPressed: () {
-                model.popWithPassword(model.password);
-              },
-              tooltip: context.getString("done_tooltip"),
-              icon: Icon(Feather.check_circle),
-            ),
-          ],
         ),
-        body: ListView(
-          physics: const AlwaysScrollableScrollPhysics(
-            parent: const BouncingScrollPhysics(),
+        leading: IconButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          tooltip: context.getString("back_tooltip"),
+          icon: Icon(Feather.x_circle),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.of(context).pop(password);
+            },
+            tooltip: context.getString("done_tooltip"),
+            icon: Icon(Feather.check_circle),
           ),
-          padding: const EdgeInsets.symmetric(
-            horizontal: 16,
+        ],
+      ),
+      body: ListView(
+        physics: const AlwaysScrollableScrollPhysics(
+          parent: const BouncingScrollPhysics(),
+        ),
+        padding: const EdgeInsets.symmetric(
+          horizontal: 16,
+        ),
+        children: [
+          SizedBox(
+            height: 8,
           ),
-          children: [
-            SizedBox(
-              height: 8,
+          Text(
+            password,
+            style: TextStyle(
+              fontSize: 16.0,
+              fontWeight: FontWeight.bold,
             ),
-            Text(
-              model.password,
-              style: TextStyle(
-                fontSize: 16.0,
-                fontWeight: FontWeight.bold,
-              ),
+          ),
+          SizedBox(
+            height: 12,
+          ),
+          SwitchListTile(
+            contentPadding: const EdgeInsets.symmetric(
+              horizontal: 0,
             ),
-            SizedBox(
-              height: 12,
-            ),
-            SwitchListTile(
-              contentPadding: const EdgeInsets.symmetric(
-                horizontal: 0,
-              ),
-              value: model.diceware,
-              onChanged: (val) {
-                model.diceware = val;
+            value: diceware,
+            onChanged: (val) {
+              setState(() {
+                diceware = val;
+
                 if (val) {
-                  model.words = 5;
+                  words = 5;
                   wordController.text = "5";
                 } else {
-                  model.words = 16;
+                  words = 16;
                   wordController.text = "16";
                 }
-                model.getPassword(
-                  length: model.words,
-                  capitalize: model.capitalize,
-                );
-              },
-              title: Text(
-                context.getString("memorizable"),
-              ),
+              });
+
+              getPassword(
+                length: words,
+                capitalize: capitalize,
+              );
+            },
+            title: Text(
+              context.getString("memorizable"),
             ),
-            SizedBox(
-              height: 4,
+          ),
+          SizedBox(
+            height: 4,
+          ),
+          TextFormField(
+            controller: wordController,
+            decoration: InputDecoration(
+              labelText: diceware
+                  ? context.getString("no_words").toUpperCase()
+                  : context.getString("length").toUpperCase(),
             ),
-            TextFormField(
-              controller: wordController,
-              decoration: InputDecoration(
-                labelText: model.diceware
-                    ? context.getString("no_words").toUpperCase()
-                    : context.getString("length").toUpperCase(),
-              ),
-              inputFormatters: [
-                FilteringTextInputFormatter.digitsOnly,
-              ],
-              keyboardType: TextInputType.number,
-              onFieldSubmitted: (val) {
-                model.getPassword(
-                  length: model.words,
-                  capitalize: model.capitalize,
-                );
-              },
-            ),
-            SizedBox(
-              height: 12,
-            ),
-            model.diceware
-                ? Column(
-                    children: [
-                      SwitchListTile(
-                        contentPadding: const EdgeInsets.symmetric(
-                          horizontal: 0,
-                        ),
-                        value: model.capitalize,
-                        onChanged: (val) {
-                          model.capitalize = val;
-                          model.getPassword(
-                            length: model.words,
-                            capitalize: model.capitalize,
-                          );
-                        },
-                        title: Text(
-                          context.getString("capitalize"),
-                        ),
+            inputFormatters: [
+              FilteringTextInputFormatter.digitsOnly,
+            ],
+            keyboardType: TextInputType.number,
+            onFieldSubmitted: (val) {
+              getPassword(
+                length: words,
+                capitalize: capitalize,
+              );
+            },
+          ),
+          SizedBox(
+            height: 12,
+          ),
+          diceware
+              ? Column(
+                  children: [
+                    SwitchListTile(
+                      contentPadding: const EdgeInsets.symmetric(
+                        horizontal: 0,
                       ),
-                      SizedBox(
-                        height: 4,
+                      value: capitalize,
+                      onChanged: (val) {
+                        setState(() {
+                          capitalize = val;
+                        });
+
+                        getPassword(
+                          length: words,
+                          capitalize: capitalize,
+                        );
+                      },
+                      title: Text(
+                        context.getString("capitalize"),
                       ),
-                    ],
-                  )
-                : Container(),
-            Button(
-              child: Text(
-                context.getString("regenrate_password"),
-              ),
-              onClick: () {
-                model.getPassword(
-                  length: model.words,
-                  capitalize: model.capitalize,
-                );
-              },
+                    ),
+                    SizedBox(
+                      height: 4,
+                    ),
+                  ],
+                )
+              : Container(),
+          Button(
+            child: Text(
+              context.getString("regenrate_password"),
             ),
-            SizedBox(
-              height: 12,
-            ),
-          ],
-        ),
+            onClick: () {
+              getPassword(
+                length: words,
+                capitalize: capitalize,
+              );
+            },
+          ),
+          SizedBox(
+            height: 12,
+          ),
+        ],
       ),
     );
   }
