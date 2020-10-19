29793955572c98da1ee4eef5715ce3347609d4a2
Akshit Garg <garg.akshit@gmail.com>
2020-10-20 00:40:08 +0530

    [GetStartedScreen] Move off stacked

--- lib/screens/get_started/get_started_screen.dart
+++ lib/screens/get_started/get_started_screen.dart
@@ -1,36 +1,38 @@
 import 'package:ez_localization/ez_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:stacked/stacked.dart';
 import 'package:supercharged/supercharged.dart';
 
 import '../../widgets/title.dart';
-import 'get_started_viewmodel.dart';
+import '../set_pin/set_pin_screen.dart';
 
 class GetStartedScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ViewModelBuilder<GetStartedViewModel>.nonReactive(
-      viewModelBuilder: () => GetStartedViewModel(),
-      builder: (context, model, child) => Scaffold(
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            TitleWidget(),
-            SizedBox(
-              height: 32,
-            ),
-            RaisedButton(
-              onPressed: model.handleClick,
-              child: Text(
-                context.getString("get_started"),
-                style: TextStyle(
-                  color: "#121212".toColor(),
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          TitleWidget(),
+          SizedBox(
+            height: 32,
+          ),
+          RaisedButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (_) => SetPinScreen(),
                 ),
+              );
+            },
+            child: Text(
+              context.getString("get_started"),
+              style: TextStyle(
+                color: "#121212".toColor(),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
