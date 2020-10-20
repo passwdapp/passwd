2e53b70e1f656ec475efc67972524297c6adef56
Akshit Garg <garg.akshit@gmail.com>
2020-10-20 14:47:27 +0530

    [HomeTagsScreen] Move off stacked

--- lib/screens/home_tags/home_tags_screen.dart
+++ lib/screens/home_tags/home_tags_screen.dart
@@ -1,56 +1,60 @@
 import 'package:flutter/material.dart';
-import 'package:stacked/stacked.dart';
+import 'package:provider/provider.dart';
 
 import '../../constants/colors.dart';
+import '../../redux/appstate.dart';
 import '../../widgets/home_list_item.dart';
-import 'home_tags_viewmodel.dart';
 
 // Navigation Item, not to be navigated to
 // So not injected in auto_route
-class HomeTagsScreen extends StatelessWidget {
+class HomeTagsScreen extends StatefulWidget {
+  @override
+  _HomeTagsScreenState createState() => _HomeTagsScreenState();
+}
+
+class _HomeTagsScreenState extends State<HomeTagsScreen> {
   @override
   Widget build(BuildContext context) {
-    return ViewModelBuilder<HomeTagsViewModel>.reactive(
-      viewModelBuilder: () => HomeTagsViewModel(),
-      builder: (context, model, child) => model.entries.entries.length != 0
-          ? ListView.builder(
-              itemCount: model.tags.length,
-              itemBuilder: (context, i) => ExpansionTile(
-                title: Row(
-                  children: [
-                    Container(
-                      height: 16,
-                      width: 16,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(32),
-                        color: tagColors[model.tags[i].color].color,
-                      ),
+    final state = Provider.of<AppState>(context);
+
+    return state.entries.entries.length != 0
+        ? ListView.builder(
+            itemCount: state.entries.tags.length,
+            itemBuilder: (context, i) => ExpansionTile(
+              title: Row(
+                children: [
+                  Container(
+                    height: 16,
+                    width: 16,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(32),
+                      color: tagColors[state.entries.tags[i].color].color,
                     ),
-                    SizedBox(
-                      width: 16,
-                    ),
-                    Text(model.tags[i].name),
-                  ],
-                ),
-                children: model.entries.entries
-                    .where((entry) {
-                      return entry.tags.indexOf(model.tags[i].id) != -1;
-                    })
-                    .map(
-                      (e) => HomeListItem(entry: e),
-                    )
-                    .toList(),
+                  ),
+                  SizedBox(
+                    width: 16,
+                  ),
+                  Text(state.entries.tags[i].name),
+                ],
               ),
-            )
-          : Center(
-              child: Text(
-                "no_tags",
-                style: Theme.of(context).textTheme.headline5.copyWith(
-                      color: Colors.white.withOpacity(0.6),
-                    ),
-              ),
+              children: state.entries.entries
+                  .where((entry) {
+                    return entry.tags.indexOf(state.entries.tags[i].id) != -1;
+                  })
+                  .map(
+                    (e) => HomeListItem(entry: e),
+                  )
+                  .toList(),
             ),
-    );
+          )
+        : Center(
+            child: Text(
+              "no_tags",
+              style: Theme.of(context).textTheme.headline5.copyWith(
+                    color: Colors.white.withOpacity(0.6),
+                  ),
+            ),
+          );
   }
 }
 
