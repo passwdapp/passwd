53fafc31135324af4690ad7a890084430ba65e44
Akshit Garg <garg.akshit@gmail.com>
2020-10-20 13:31:28 +0530

    [VerifyPinScreen] Move off stacked

--- lib/screens/verify_pin/verify_pin_screen.dart
+++ lib/screens/verify_pin/verify_pin_screen.dart
@@ -1,74 +1,127 @@
 import 'package:ez_localization/ez_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:stacked/stacked.dart';
 import 'package:supercharged/supercharged.dart';
 
+import '../../models/biometrics_result.dart';
+import '../../services/authentication/authentication_service.dart';
+import '../../services/biometrics/biometrics_service.dart';
+import '../../services/locator.dart';
 import '../../widgets/pin_input.dart';
-import 'verify_pin_viewmodel.dart';
+import '../home/home_screen.dart';
 
-class VerifyPinScreen extends StatelessWidget {
+class VerifyPinScreen extends StatefulWidget {
+  @override
+  _VerifyPinScreenState createState() => _VerifyPinScreenState();
+}
+
+class _VerifyPinScreenState extends State<VerifyPinScreen> {
+  String error = "";
+
+  Future<bool> biometricsAvailable() async {
+    if (await locator<AuthenticationService>().allowBiometrics() &&
+        await locator<BiometricsService>().biometricsAvailable()) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  Future tryBiometrics() async {
+    if (await biometricsAvailable()) {
+      BiometricsResult result = await locator<BiometricsService>()
+          .authenticate("To unlock your vault.");
+
+      if (result == BiometricsResult.AUTHENTICATED) {
+        replace();
+      }
+    }
+  }
+
+  Future verifyPin(int pin) async {
+    bool valid = await locator<AuthenticationService>().comparePin(pin);
+
+    if (valid) {
+      replace();
+    } else {
+      setState(() {
+        error = "Invalid pin entered";
+      });
+    }
+  }
+
+  void replace() {
+    Navigator.of(context).pushAndRemoveUntil(
+      MaterialPageRoute(
+        builder: (_) => HomeScreen(),
+      ),
+      (route) => false,
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    tryBiometrics();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return ViewModelBuilder<VerifyPinViewModel>.reactive(
-      viewModelBuilder: () => VerifyPinViewModel(),
-      builder: (context, model, child) => Scaffold(
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(
-              model.error.isEmpty
-                  ? context.getString("enter_pin")
-                  : model.error,
-              style: Theme.of(context).textTheme.headline5.copyWith(
-                    fontWeight: FontWeight.w900,
-                    color: model.error.isEmpty
-                        ? Theme.of(context).textTheme.headline5.color
-                        : Colors.red[300],
-                  ),
-            ),
-            SizedBox(
-              height: 32,
-            ),
-            PinInputWidget(
-              onSubmit: (String value) {
-                model.pin = value.toInt();
-              },
-            ),
-            FutureBuilder<bool>(
-              future: model.biometricsAvailable(),
-              builder: (context, snapshot) {
-                if (snapshot.hasData && snapshot.data) {
-                  return Column(
-                    children: [
-                      SizedBox(
-                        height: 6,
-                      ),
-                      Container(
-                        width: 284,
-                        child: ListTile(
-                          leading: Icon(
-                            Icons.fingerprint_outlined,
-                          ),
-                          contentPadding: const EdgeInsets.all(0),
-                          trailing: Text(
-                            context.getString("biometrics_retry"),
-                            style: Theme.of(context).textTheme.bodyText1,
-                          ),
-                          onTap: () {
-                            model.tryBiometrics();
-                          },
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(
+            error.isEmpty ? context.getString("enter_pin") : error,
+            style: Theme.of(context).textTheme.headline5.copyWith(
+                  fontWeight: FontWeight.w900,
+                  color: error.isEmpty
+                      ? Theme.of(context).textTheme.headline5.color
+                      : Colors.red[300],
+                ),
+          ),
+          SizedBox(
+            height: 32,
+          ),
+          PinInputWidget(
+            onSubmit: (String value) {
+              verifyPin(value.toInt());
+            },
+          ),
+          FutureBuilder<bool>(
+            future: biometricsAvailable(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData && snapshot.data) {
+                return Column(
+                  children: [
+                    SizedBox(
+                      height: 6,
+                    ),
+                    Container(
+                      width: 284,
+                      child: ListTile(
+                        leading: Icon(
+                          Icons.fingerprint_outlined,
                         ),
+                        contentPadding: const EdgeInsets.all(0),
+                        trailing: Text(
+                          context.getString("biometrics_retry"),
+                          style: Theme.of(context).textTheme.bodyText1,
+                        ),
+                        onTap: () {
+                          tryBiometrics();
+                        },
                       ),
-                    ],
-                  );
-                }
+                    ),
+                  ],
+                );
+              }
 
-                return Container();
-              },
-            ),
-          ],
-        ),
+              return Container();
+            },
+          ),
+        ],
       ),
     );
   }
