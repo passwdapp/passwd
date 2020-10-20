29fd2143c298a84185d3f4fffe01a94f607c938d
Akshit Garg <garg.akshit@gmail.com>
2020-10-20 14:44:37 +0530

    [SetPinScreen] Move off stacked

--- lib/screens/set_pin/set_pin_screen.dart
+++ lib/screens/set_pin/set_pin_screen.dart
@@ -1,84 +1,129 @@
 import 'package:ez_localization/ez_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:stacked/stacked.dart';
 import 'package:supercharged/supercharged.dart';
 
+import '../../models/biometrics_result.dart';
+import '../../services/authentication/authentication_service.dart';
+import '../../services/biometrics/biometrics_service.dart';
+import '../../services/locator.dart';
 import '../../widgets/pin_input.dart';
-import 'set_pin_viewmodel.dart';
+import '../home/home_screen.dart';
 
-class SetPinScreen extends StatelessWidget {
+class SetPinScreen extends StatefulWidget {
+  @override
+  _SetPinScreenState createState() => _SetPinScreenState();
+}
+
+class _SetPinScreenState extends State<SetPinScreen> {
+  int pin;
+  bool nextEnabled = false;
+  bool biometrics = false;
+
+  Future<void> setBiometrics(bool value) async {
+    if (value) {
+      BiometricsResult result = await locator<BiometricsService>()
+          .authenticate("Initialize biometrics");
+
+      if (result == BiometricsResult.AUTHENTICATED) {
+        biometrics = true;
+      } else {
+        biometrics = false;
+      }
+    } else {
+      biometrics = false;
+    }
+
+    setState(() {});
+
+    await locator<AuthenticationService>().writeBiometrics(biometrics);
+  }
+
+  Future<bool> biometricsAvailable() async {
+    return await locator<BiometricsService>().biometricsAvailable();
+  }
+
+  Future next() async {
+    await locator<AuthenticationService>().writePin(pin);
+    Navigator.of(context).pushAndRemoveUntil(
+      MaterialPageRoute(
+        builder: (_) => HomeScreen(),
+      ),
+      (route) => false,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return ViewModelBuilder<SetPinViewModel>.reactive(
-      viewModelBuilder: () => SetPinViewModel(),
-      builder: (context, model, child) => Scaffold(
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(
-              context.getString("set_pin"),
-              style: Theme.of(context).textTheme.headline5.copyWith(
-                    fontWeight: FontWeight.w900,
-                  ),
-            ),
-            SizedBox(
-              height: 32,
-            ),
-            PinInputWidget(
-              onSubmit: (String value) {
-                model.pin = value.toInt();
-              },
-            ),
-            FutureBuilder<bool>(
-              future: model.biometricsAvailable(),
-              builder: (context, snapshot) {
-                if (snapshot.hasData && snapshot.data) {
-                  return Column(
-                    children: [
-                      SizedBox(
-                        height: 6,
-                      ),
-                      Container(
-                        width: 284,
-                        child: ListTile(
-                          leading: Icon(
-                            Icons.fingerprint_outlined,
-                          ),
-                          contentPadding: const EdgeInsets.all(0),
-                          title: Text(
-                            context.getString("enable_biometrics"),
-                          ),
-                          trailing: Switch(
-                            value: model.biometrics,
-                            onChanged: (bool value) {
-                              model.setBiometrics(value);
-                            },
-                          ),
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(
+            context.getString("set_pin"),
+            style: Theme.of(context).textTheme.headline5.copyWith(
+                  fontWeight: FontWeight.w900,
+                ),
+          ),
+          SizedBox(
+            height: 32,
+          ),
+          PinInputWidget(
+            onSubmit: (String value) {
+              setState(() {
+                pin = value.toInt();
+                nextEnabled = true;
+              });
+            },
+          ),
+          FutureBuilder<bool>(
+            future: biometricsAvailable(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData && snapshot.data) {
+                return Column(
+                  children: [
+                    SizedBox(
+                      height: 6,
+                    ),
+                    Container(
+                      width: 284,
+                      child: ListTile(
+                        leading: Icon(
+                          Icons.fingerprint_outlined,
+                        ),
+                        contentPadding: const EdgeInsets.all(0),
+                        title: Text(
+                          context.getString("enable_biometrics"),
+                        ),
+                        trailing: Switch(
+                          value: biometrics,
+                          onChanged: (bool value) {
+                            setBiometrics(value);
+                          },
                         ),
                       ),
-                    ],
-                  );
-                }
+                    ),
+                  ],
+                );
+              }
 
-                return Container();
-              },
-            ),
-          ],
-        ),
-        floatingActionButton: FloatingActionButton.extended(
-          onPressed: model.nextEnabled
-              ? () {
-                  model.next();
-                }
-              : null,
-          label: Text(
-            context.getString("next"),
+              return Container();
+            },
           ),
-          icon: Icon(Icons.chevron_right),
-          disabledElevation: 0.0,
-          elevation: 52.0,
+        ],
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: nextEnabled
+            ? () {
+                next();
+              }
+            : null,
+        label: Text(
+          context.getString("next"),
         ),
+        icon: Icon(Icons.chevron_right),
+        disabledElevation: 0.0,
+        elevation: 52.0,
       ),
     );
   }
