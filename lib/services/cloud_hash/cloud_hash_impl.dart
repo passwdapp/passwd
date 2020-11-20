3ba8f241346ac4e049797975f080a02d4811bdea
Akshit Garg <garg.akshit@gmail.com>
2020-11-20 19:28:32 +0530

    [Cloud] Add HKDF to deriveSyncEncryptionPassword

--- lib/services/cloud_hash/cloud_hash_impl.dart
+++ lib/services/cloud_hash/cloud_hash_impl.dart
@@ -1,20 +1,30 @@
-import 'package:crypto/crypto.dart';
+import 'dart:convert';
+
+import 'package:crypto/crypto.dart' as crypto;
+import 'package:cryptography/cryptography.dart';
 import 'package:passwd/services/cloud_hash/cloud_hash_service.dart';
 
 class CloudHashImpl implements CloudHashService {
   @override
-  String deriveSyncEncryptionPassword(String username, String password) {
+  Future<String> deriveSyncEncryptionPassword(
+    String username,
+    String password,
+  ) async {
     final usernameBytes = username.codeUnits;
     final passwordBytes = password.codeUnits;
 
-    final hmac = Hmac(sha512, usernameBytes);
+    final hmac = crypto.Hmac(crypto.sha512, usernameBytes);
     var digest = hmac.convert(passwordBytes);
 
     for (var i = 0; i < 1024; i++) {
       digest = hmac.convert(digest.bytes);
     }
 
-    return digest.toString();
+    final hkdf = Hkdf(Hmac(sha512));
+    final input = SecretKey(digest.toString().codeUnits);
+    final output = await hkdf.deriveKey(input, outputLength: 32);
+
+    return base64.encode(await output.extract());
   }
 
   @override
@@ -22,7 +32,7 @@ class CloudHashImpl implements CloudHashService {
     final usernameBytes = username.codeUnits;
     final passwordBytes = password.codeUnits;
 
-    final hmac = Hmac(sha512, usernameBytes);
+    final hmac = crypto.Hmac(crypto.sha512, usernameBytes);
     var digest = hmac.convert(passwordBytes);
 
     for (var i = 0; i < 512; i++) {
--- lib/services/cloud_hash/cloud_hash_service.dart
+++ lib/services/cloud_hash/cloud_hash_service.dart
@@ -1,4 +1,4 @@
 abstract class CloudHashService {
   String deriveSyncPassword(String username, String password);
-  String deriveSyncEncryptionPassword(String username, String password);
+  Future<String> deriveSyncEncryptionPassword(String username, String password);
 }
