ab52e36a606aa6ff81a468603b9a2ff7cbe42fa0
Akshit Garg <garg.akshit@gmail.com>
2020-11-27 14:41:13 +0530

    [Constants] Add KV keys for sync

--- lib/constants/api.dart
+++ lib/constants/api.dart
@@ -1,1 +1,10 @@
 const supportedApiVersion = 'v1';
+
+const SECRETKEY_KEY = 'SYNC_SECRETKEY';
+const URI_KEY = 'SYNC_URI';
+
+const USERNAME_KEY = 'SYNC_USERNAME';
+const HASH_KEY = 'SYNC_HASH';
+
+const ACCESS_TOKEN_KEY = 'SYNC_ACCESS_TOKEN';
+const REFRESH_TOKEN_KEY = 'SYNC_REFRESH_TOKEN';
