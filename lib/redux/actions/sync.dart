9198662fb37794a06b41ea37164a0dffcd320a79
Akshit Garg <garg.akshit@gmail.com>
2020-11-28 01:14:47 +0530

    [Redux] Add auth actions

--- lib/redux/actions/sync.dart
+++ lib/redux/actions/sync.dart
@@ -1,6 +1,7 @@
 import 'package:async_redux/async_redux.dart';
 
 import '../../constants/api.dart';
+import '../../services/cloud_users/cloud_users_service.dart';
 import '../../services/locator.dart';
 import '../../services/secure_kv/secure_kv.dart';
 import '../appstate.dart';
@@ -14,5 +15,39 @@ class CheckLoginAction extends ReduxAction<AppState> {
     return state.copyWith(
       isLoggedIn: accessToken != null && accessToken != '',
     );
+  }
+}
+
+class LoginAction extends ReduxAction<AppState> {
+  final Uri uri;
+  final String secret;
+  final String username;
+  final String password;
+
+  LoginAction(this.uri, this.secret, this.username, this.password);
+
+  @override
+  Future<AppState> reduce() async {
+    final cloudUsersService = locator<CloudUsersService>();
+    await cloudUsersService.login(username, password, secret, uri);
+
+    return state;
+  }
+}
+
+class RegisterAction extends ReduxAction<AppState> {
+  final Uri uri;
+  final String secret;
+  final String username;
+  final String password;
+
+  RegisterAction(this.uri, this.secret, this.username, this.password);
+
+  @override
+  Future<AppState> reduce() async {
+    final cloudUsersService = locator<CloudUsersService>();
+    await cloudUsersService.register(username, password, secret, uri);
+
+    return state;
   }
 }
