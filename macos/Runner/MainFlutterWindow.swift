1a010d06c62094f8c1abcff14fbded0629f88b25
Akshit Garg <garg.akshit@gmail.com>
2021-02-08 19:00:57 +0530

    [MacOS] Fixes for signing

--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -7,7 +7,7 @@ class MainFlutterWindow: NSWindow {
     let windowFrame = self.frame
     self.contentViewController = flutterViewController
     self.setFrame(windowFrame, display: true)
-
+    
     RegisterGeneratedPlugins(registry: flutterViewController)
 
     super.awakeFromNib()
