dbdef05a844e17c0d517c8377495272c83ee45bb
Akshit Garg <garg.akshit@gmail.com>
2021-02-18 20:55:02 +0530

    [CloudSync] Add more checks to prevent race conditions
    This still isn't perfect and needs a lot of testing

--- lib/services/cloud_sync/cloud_sync_impl.dart
+++ lib/services/cloud_sync/cloud_sync_impl.dart
@@ -33,6 +33,7 @@ class CloudSyncImpl implements CloudSyncService {
   static const uploadEndpoint =
       '$supportedApiVersion$uploadsSuffix$uploadSuffix';
 
+  // success, nonce
   Future<Tuple2<bool, String>> fetchNonce(
     Uri endpoint,
     String secretKey,
@@ -58,6 +59,7 @@ class CloudSyncImpl implements CloudSyncService {
     }
   }
 
+  // uri, secret key, access token
   Future<Tuple3<Uri, String, String>> getStoredData() async {
     final endpoint = Uri.parse(await secureKVService.getValue(URI_KEY));
     final secretKey = await secureKVService.getValue(SECRETKEY_KEY);
@@ -67,9 +69,9 @@ class CloudSyncImpl implements CloudSyncService {
 
     final currentTimestamp = DateTime.now().millisecondsSinceEpoch;
 
-    // Refresh token is the current token is older than 55 mins
+    // Refresh token is the current token is older than 50 mins
     // Access tokens expire every 1hr
-    if (tokenIssueTime + (55 * 60 * 1000) <= currentTimestamp) {
+    if (tokenIssueTime + (50 * 60 * 1000) <= currentTimestamp) {
       final refreshToken = await secureKVService.getValue(REFRESH_TOKEN_KEY);
       await cloudUsersService.refresh(refreshToken, secretKey, endpoint);
     }
@@ -94,21 +96,70 @@ class CloudSyncImpl implements CloudSyncService {
     final nonceTuple = await fetchNonce(endpoint, secretKey, accessToken);
     if (!nonceTuple.item1) {
       Loggers.syncLogger.severe('Remote nonce fetch failed');
-    } else {
-      final nonce = nonceTuple.item2;
-      final storedNonce = await secureKVService.getValue(NONCE_KEY);
+      Loggers.syncLogger.severe(
+          'This usually happens when the server has no DB, so allowing push');
+    }
+    final nonce = nonceTuple.item2;
+    final storedNonce = await secureKVService.getValue(NONCE_KEY);
 
-      if (nonce == storedNonce) {
+    // old nonce checking ahead
+    // new nonce checking is implemented below
+    // if (storedNonce != null && storedNonce != '' && nonce == storedNonce) {
+    //   Loggers.syncLogger.info(
+    //       'Remote nonce same as local nonce, cancelling merge, cancelling push...');
+
+    //   return Tuple4(
+    //     localEntries,
+    //     true,
+    //     true,
+    //     false,
+    //   );
+    // }
+
+    final nonceInt = int.tryParse(nonce);
+    if (nonceInt == null) {
+      Loggers.syncLogger.info(
+          'Looks like the server still has the old "random" nonce, cancelling merge, allowing push to get the server to the new "sequential" format');
+
+      return Tuple4(
+        localEntries,
+        false,
+        true,
+        true,
+      );
+    }
+
+    final storedNonceInt = int.tryParse(storedNonce);
+    if (storedNonceInt == null) {
+      Loggers.syncLogger.info(
+          'Looks like the server has the new "sequential" nonce while the client has the old "random" nonce. Forcing a remote merge while cancelling push as its guaranteed that the server will have a newer version');
+    } else {
+      Loggers.syncLogger.info(
+          'Looks like both the client and the server have a "sequential" nonce, proceeding with the normal procedure');
+    }
+
+    final storedUpdateTimestamp =
+        await secureKVService.getValue(UPLOAD_TIMESTAMP_KEY);
+    final localUpdateTimestamp = localEntries.lastUpdated.toString();
+
+    if (storedNonceInt == nonceInt) {
+      if (storedUpdateTimestamp == localUpdateTimestamp) {
         Loggers.syncLogger.info(
-            'Remote nonce nonce same as local nonce, cancelling merge, allowing push...');
+            'Both the nonce and the timestamp are the same. This means both the remote and the local DB is the same, cancelling merge, canelling sync');
 
         return Tuple4(
           localEntries,
           true,
           true,
-          true,
+          false,
         );
+      } else {
+        Loggers.syncLogger.warning(
+            'Weird situation where the nonce is the same but the timestamp is not. Proceeding with sync');
       }
+    } else {
+      Loggers.syncLogger.info(
+          'Stored nonce is not equal to the remote nonce. This requires a full sync and merge');
     }
 
     try {
@@ -123,10 +174,15 @@ class CloudSyncImpl implements CloudSyncService {
       final decryptedData = await cloudEncryptionService.decrypt(response.data);
 
       if (localEntries.lastUpdated == null ||
-          localEntries.lastUpdated < decryptedData.lastUpdated) {
+          localEntries.lastUpdated <= decryptedData.lastUpdated) {
         Loggers.syncLogger.info('Merge successful, cancelling push');
         await databaseService.setEntries(decryptedData);
-        return Tuple4(decryptedData, false, true, false);
+        return Tuple4(
+          decryptedData,
+          false,
+          true,
+          false,
+        );
       }
 
       Loggers.syncLogger.info(
@@ -180,6 +236,12 @@ class CloudSyncImpl implements CloudSyncService {
           'db': MultipartFile.fromBytes(encryptedData, filename: 'db1.passwd1'),
         }),
       );
+
+      await secureKVService.putValue(
+          UPLOAD_TIMESTAMP_KEY,
+          (databaseService.entries.lastUpdated ??
+                  DateTime.now().millisecondsSinceEpoch)
+              .toString());
 
       final nonce = response.data['nonce'].toString();
       await secureKVService.putValue(NONCE_KEY, nonce);
